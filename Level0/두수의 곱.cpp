91a4b8cbe377bee45f43b2cb16a13d29764c2412
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-03-03 12:58:14 +0900

    Update: 230303
    commit 3 solutions.

---
+++ Level0/두수의 곱.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//정수 num1, num2가 매개변수 주어집니다. 
+//num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+int solution(int num1, int num2)
+{
+    return num1 * num2;
+}
---
+++ Level0/두수의 차.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+//정수 num1과 num2가 주어질 때,
+//num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+int solution(int num1, int num2)
+{
+	return num1 - num2;
+}
