ea1c3a5c9fa41c03ea2f3a68e3abc7f62610875c
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-03-08 08:57:58 +0900

    Update: 230308
    commit 3 solutions.

---
+++ Level0/두 수의 합.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+//정수 num1과 num2가 주어질 때,
+//num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+int solution(int num1, int num2) {
+    return num1 + num2;
+}
