8f60b1b150fc8934b098403f3acdb7c2fdd00b09
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-10-22 13:21:45 +0900

    Update: 231022
    commit 6 solutions.

---
+++ Level1/문자열을 정수로 바꾸기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+*/
+
+int solution(string s)
+{
+    int answer = 0;
+
+    answer = stoi(s);
+
+    return answer;
+}
---
+++ Level1/자연수를 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 
+예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다
+*/
+
+vector<int> solution(long long n)
+{
+    vector<int> answer;
+
+    while (n > 0)
+    {
+        answer.push_back(n % 10);
+        n = n / 10;
+    }
+
+    return answer;
+}
