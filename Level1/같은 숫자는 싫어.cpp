4daf1b29ed7a9c8e122608da57b8d45ad8534287
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-10-26 13:11:19 +0900

    Update: 231026
    commit 3 solutions.

---
+++ Level1/같은 숫자는 싫어.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr)
+{
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+
+    return arr;
+}
