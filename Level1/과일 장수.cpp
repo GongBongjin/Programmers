70853d69ff30275b760585f1b6cf25503e17cca2
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-11-28 14:58:23 +0900

    Update: 231128
    commit 2 solutions.

---
+++ Level1/과일 장수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int k, int m, vector<int> score)
+{
+    int answer = 0;
+
+    sort(score.begin(), score.end(), greater<>());
+
+    for (int i = 0; i < score.size() / m; i++)
+    {
+        vector<int> v(score.begin() + i * m, score.begin() + (i + 1) * m);
+        answer += v[v.size() - 1] * m;
+    }
+
+    return answer;
+}
