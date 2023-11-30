4cda2281ba35786dd424d43913d44c6fb7adc458
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-11-30 14:46:04 +0900

    Update: 231130
    commit 2 solutions.

--- Level0/꼬리 문자열.cpp
+++ Level0/꼬리 문자열.cpp
@@ -3,32 +3,15 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> answers)
+string solution(vector<string> str_list, string ex)
 {
-    vector<int> answer;
+    string answer = "";
 
-    int len = answers.size();
-
-    int one[5] = { 1, 2, 3, 4, 5 };
-    int two[8] = { 2, 1, 2, 3, 2, 4, 2, 5 };
-    int thr[10] = { 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 };
-    int oneCnt = 0;
-    int twoCnt = 0;
-    int thrCnt = 0;
-
-    for (int i = 0; i < len; i++)
-    {
-        if (one[i % 5] == answers[i]) oneCnt++;
-        if (two[i % 8] == answers[i]) twoCnt++;
-        if (thr[i % 10] == answers[i]) thrCnt++;
-    }
-
-    int maxCnt = max(oneCnt, twoCnt);
-    maxCnt = max(maxCnt, thrCnt);
-
-    if (maxCnt == oneCnt) answer.push_back(1);
-    if (maxCnt == twoCnt) answer.push_back(2);
-    if (maxCnt == thrCnt) answer.push_back(3);
+    for (string str : str_list)
+        if (str.find(ex) == string::npos)
+        {
+            answer += str;
+        }
 
     return answer;
 }
