40d3e2532b3d74de37cfcf0b5927a96e54399440
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-12-01 15:30:26 +0900

    Update: 231201
    commit 3 solutions.

---
+++ Level0/ad 제거하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr)
+{
+    vector<string> answer;
+
+    for (int i = 0; i < strArr.size(); ++i)
+    {
+        if (strArr[i].find("ad") == string::npos)
+            answer.push_back(strArr[i]);
+    }
+
+    return answer;
+}
---
+++ Level0/특별한 이차원 배열 2.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        for (int j = 0; j < arr.size(); j++)
+        {
+            if (arr[i][j] != arr[j][i]) return 0;
+        }
+    }
+    return 1;
+}
