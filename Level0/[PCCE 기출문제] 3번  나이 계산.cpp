0157bdf4e3e4a3dd07b43c43a8f176755e12a2d7
Gong Bongjin <113762622+GongBongjin@users.noreply.github.com>
2023-12-06 17:56:07 +0900

    Update: 231206
    commit 3 solutions.

---
+++ Level0/[PCCE 기출문제] 3번  나이 계산.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int year, answer;
+    string age_type;
+    cin >> year >> age_type;
+
+    if (age_type == "Korea") 
+    {
+        answer = 2030 - year + 1;
+    }
+    else if (age_type == "Year")
+    {
+        answer = 2030 - year;
+    }
+
+    cout << answer << endl;
+    return 0;
+}
